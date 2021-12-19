c25a7f4418729237ac2036afef90b89b8a2e6130
chirag.pimpale <chiragpimple7@gmail.com>
2021-12-19 21:59:39 +0000

    Changed the test case name

---
+++ WareHouseMgmt/src/test/java/com/dbs/test/WareHouseApplicationTest.java
@@ -0,0 +1,11 @@
+package com.dbs.test;
+/*
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class STemplateDbApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */

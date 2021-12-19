2dc94ee0e4b94ed0c7ce1715dc9cc9c7f379e140
chirag.pimpale <chiragpimple7@gmail.com>
2021-12-19 22:25:41 +0000

    Changed file name

--- WareHouseMgmt/src/test/java/com/dbs/test/WareHouseApplicationTest.java
+++ WareHouseMgmt/src/test/java/com/dbs/test/WareHouseApplicationTest.java
@@ -1,11 +1,13 @@
 package com.dbs.test;
-/*
- * import org.junit.jupiter.api.Test; import
- * org.springframework.boot.test.context.SpringBootTest;
- * 
- * @SpringBootTest class STemplateDbApplicationTests {
- * 
- * @Test void contextLoads() { }
- * 
- * }
- */
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class STemplateDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
